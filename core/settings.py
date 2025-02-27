be56d7a798a866b67fbf88344ebf6b9086b30969
Nurhak ŞEKERCİ <nurhaksekerci12@gmail.com>
2025-02-27 17:11:51 +0300

    collectstatic and setting

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8xxw14&&6&%(h4q&+fh-b8dkumx3og(i_7phsbvyjscb72@=()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
